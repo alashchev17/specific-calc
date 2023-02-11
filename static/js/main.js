0d4a33bf852cb5bac58cf04e05e80760a9052391
alashchev17 <andrew.lashchev15@gmail.com>
2023-02-11 23:51:33 +0100

    Added header, section and test form

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,18 @@
 // Initialization
+
+// Simple Validation of passwords
+const formButton = document.querySelector("#formButton");
+formButton.setAttribute("disabled", "disabled");
+const inputPass = document.querySelector("#password");
+const inputPassConfirm = document.querySelector("#passwordConfirm");
+
+const passwordValidation = (inputFirst, inputSecond, button) => {
+  inputSecond.addEventListener("keyup", () => {
+    if (inputFirst.value === inputSecond.value) {
+      button.removeAttribute("disabled");
+    } else if (inputFirst.value !== inputSecond.value) {
+      button.setAttribute("disabled", "disabled");
+    }
+  });
+};
+passwordValidation(inputPass, inputPassConfirm, formButton);
