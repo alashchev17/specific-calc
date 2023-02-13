39303d77ef303e5bd16b2d5faad496a0e4592af0
alashchev17 <andrew.lashchev15@gmail.com>
2023-02-13 10:24:45 +0100

    Anchors animated scroll

--- static/js/main.js
+++ static/js/main.js
@@ -23,6 +23,21 @@
   }
 })();
 
+// Animation of scrolling
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const blockID = anchor.getAttribute("href").substr(1);
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+}
 // Simple Validation of passwords
 const formButton = document.querySelector("#formButton");
 formButton.setAttribute("disabled", "disabled");
