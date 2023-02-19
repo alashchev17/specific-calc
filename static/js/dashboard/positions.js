80043fc20ab9b58624157570a38c06663caa39bc
alashchev17 <andrew.lashchev15@gmail.com>
2023-02-19 21:40:37 +0100

    Validation improvements

--- static/js/dashboard/positions.js
+++ static/js/dashboard/positions.js
@@ -32,6 +32,16 @@ function createPosition() {
 
   form.addEventListener("submit", event => {
     event.preventDefault();
+    // Проверка по всем элементам-должностям на уникальность имени
+    let allPositions = document.querySelectorAll(".positions__block-item");
+    let arrayAllPositions = Array.prototype.slice.call(allPositions);
+    for (let i = 0; i < arrayAllPositions.length; i++) {
+      if (input.value === arrayAllPositions[i].innerText) {
+        alert("Введіть унікальну назву посади!");
+        input.value = "";
+        return;
+      }
+    }
     if (input.value.trim().length === 0 || input.value.trim() === "Посад немає") {
       alert("Введіть коректну назву посади!");
     } else {
