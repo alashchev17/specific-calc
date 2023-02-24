b392105bdb9f02d04f833193edec08871e2f1a97
alashchev17 <andrew.lashchev15@gmail.com>
2023-02-24 20:06:28 +0100

    One more pre-release commit

--- static/js/dashboard/documents.js
+++ static/js/dashboard/documents.js
@@ -1,15 +1,24 @@
 const button = document.querySelector(".documents__button");
-const selectedItem = document.querySelector(".documents__select-trigger");
+const select = document.querySelector(".documents__select");
 
 button.setAttribute("disabled", "disabled");
 
-selectedItem.addEventListener("click", () => {
-  if (selectedItem.innerText !== "Оберіть посаду") {
+select.addEventListener("change", () => {
+  if (select.value !== "Оберіть посаду") {
     button.removeAttribute("disabled");
+  } else {
+    button.setAttribute("disabled", "disabled");
   }
 });
-selectedItem.addEventListener("pointerenter", () => {
-  if (selectedItem.innerText !== "Оберіть посаду") {
-    button.removeAttribute("disabled");
+
+button.addEventListener("click", e => {
+  window.preventAction = true;
+  if (select.value == "Оберіть посаду") {
+    if (window.preventAction) {
+      e.preventDefault();
+      alert("Виберіть коректну посаду!");
+      return;
+    }
   }
+  window.preventAction = false;
 });
--- static/js/dashboard/positions.js
+++ static/js/dashboard/positions.js
@@ -1,35 +1,56 @@
 function createPosition() {
   let form = document.querySelector("#positions_form");
-  let positionsBlock = document.querySelector(".positions__created");
-  let position = document.querySelector(".positions__block-item");
+  // let positionsBlock = document.querySelector(".positions__created");
+  // let position = document.querySelector(".positions__block-item");
   let input = document.querySelector(".positions__block-input");
-  let value = "";
+  // let value = "";
 
-  form.addEventListener("submit", event => {
-    event.preventDefault();
-    // Проверка по всем элементам-должностям на уникальность имени
+  function validation(form) {
     let allPositions = document.querySelectorAll(".positions__block-item");
     let arrayAllPositions = Array.prototype.slice.call(allPositions);
     for (let i = 0; i < arrayAllPositions.length; i++) {
       if (input.value === arrayAllPositions[i].innerText) {
-        alert("Введіть унікальну назву посади!");
-        input.value = "";
-        return;
+        return false;
       }
     }
     if (input.value.trim().length === 0 || input.value.trim() === "Посад немає") {
-      alert("Введіть коректну назву посади!");
-    } else {
-      if (position.innerText === "Посад немає") {
-        position.remove();
-      }
-      value = input.value;
-      let newPosition = document.createElement("span");
-      newPosition.classList.add("positions__block-item");
-      newPosition.innerText = value;
-      positionsBlock.appendChild(newPosition);
-      input.value = "";
+      return false;
     }
+    return true;
+  }
+
+  form.addEventListener("submit", event => {
+    window.preventAction = true;
+    if (validation(this) == false) {
+      if (window.preventAction) {
+        event.preventDefault();
+        // Проверка по всем элементам-должностям на уникальность имени
+        let allPositions = document.querySelectorAll(".positions__block-item");
+        let arrayAllPositions = Array.prototype.slice.call(allPositions);
+        for (let i = 0; i < arrayAllPositions.length; i++) {
+          if (input.value === arrayAllPositions[i].innerText) {
+            alert("Введіть унікальну назву посади!");
+            input.value = "";
+            return;
+          }
+        }
+        if (input.value.trim().length === 0 || input.value.trim() === "Посад немає") {
+          alert("Введіть коректну назву посади!");
+        }
+      }
+    }
+    window.preventAction = false;
+    // else {
+    //   if (position.innerText === "Посад немає") {
+    //     position.remove();
+    //   }
+    //   value = input.value;
+    //   let newPosition = document.createElement("span");
+    //   newPosition.classList.add("positions__block-item");
+    //   newPosition.innerText = value;
+    //   positionsBlock.appendChild(newPosition);
+    //   input.value = "";
+    // }
   });
 }
 
